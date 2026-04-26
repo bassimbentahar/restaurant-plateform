91ffdd94261193f3d318d9be958dabe26f55790d
Elhassania Nour Aissaoui <h4ss4ni4@gmail.com>
2026-04-26 23:47:12 +0200

    fix dev aws api url

--- frontend/client-app/src/app/services/product.service.ts
+++ frontend/client-app/src/app/services/product.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private readonly apiUrl = 'http://localhost:8080/api/v1/products';
-
+  private readonly apiUrl = `${environment.apiUrl}/api/v1/products`;
   constructor(private readonly http: HttpClient) {}
 
   getAllProducts(): Observable<Product[]> {
